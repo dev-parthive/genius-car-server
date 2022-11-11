77e3c3781eed246e8d90319ae543005f28100106
dev-parthive <dev-parthive>
2022-11-11 10:53:48 +0600

    mongodb databse added

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const cors = require('cors')
 const app  = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//require dotenv 
+require('dotenv').config()
+
+//middlewares 
+app.use(cors())
+app.use(express.json())
+// ---------------------------------
+// connect mongodb 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afdwhlk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
 app.get('/', (req, res) =>{
     res.send('Genius car server running')
 })
